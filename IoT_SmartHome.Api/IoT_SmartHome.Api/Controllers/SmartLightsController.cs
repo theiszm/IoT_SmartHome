dfcc5b29d7a6cf35a4f38bb8efa284ec1f66d0a0
theiszm <theiszm@gmail.com>
2026-05-07 17:34:11 +0800

    Added SmartLights controller with GET method.

---
+++ IoT_SmartHome.Api/IoT_SmartHome.Api/Controllers/SmartLightsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IoT_SmartHome.Api.Data;
+using IoT_SmartHome.Api.Models;
+
+namespace IoT_SmartHome.Api.Controllers
+{
+    [Route("api/[controller]")] // api/smartlights
+    [ApiController]
+    public class SmartLightsController : ControllerBase
+    {
+        private readonly HomeDbContext _context;
+
+        // Dependency injection of the HomeDbContext to access the database
+        public SmartLightsController(HomeDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/smartlights
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SmartLight>>> GetLights()
+        {
+            return await _context.SmartLights.ToListAsync();
+        }
+    }
+}
