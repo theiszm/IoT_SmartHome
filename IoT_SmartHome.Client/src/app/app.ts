a92d25ff47d96daf540e6b56e7b7e6f1a27e3959
theiszm <theiszm@gmail.com>
2026-05-10 02:23:39 +0800

    Imported 'smart-light.model'. Merged App and AppComponent. Modified to tell Component to fetch the lights when the page loads.

--- IoT_SmartHome.Client/src/app/app.ts
+++ IoT_SmartHome.Client/src/app/app.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SmartLight } from './smart-light.model';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,29 @@ import { Component, signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.css'
 })
-export class App {
+
+export class AppComponent implements OnInit {
   protected readonly title = signal('IoT_SmartHome.Client');
+
+  public lights: SmartLight[] = [];
+
+  // inject HttpClient into constructor
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit() {
+	this.getLights();
+  }
+
+  // fetches data from the .NET API
+  // "subscribe" is equivalent to Task and await in C#
+  getLights() {
+	  this.http.get<SmartLight[]>('https://localhost:7016/api/smartlights').subscribe({
+		  next: (result) => {
+			  this.lights = result;
+		  },
+		  error: (error) => {
+			  console.error('API Error:', error);
+		  }
+	  });
+  }
 }
