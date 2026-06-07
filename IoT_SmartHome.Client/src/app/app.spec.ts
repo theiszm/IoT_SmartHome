a2379919c2cc7143cde8d8fc88da3680c93b102e
theiszm <theiszm@gmail.com>
2026-06-08 06:31:35 +0800

    Referenced rather AppComponent from app.ts (./app)

--- IoT_SmartHome.Client/src/app/app.spec.ts
+++ IoT_SmartHome.Client/src/app/app.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
-import { App } from './app';
+import { AppComponent } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
@@ -9,19 +9,19 @@ describe('App', () => {
         RouterModule.forRoot([])
       ],
       declarations: [
-        App
+        AppComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, IoT_SmartHome.Client');
